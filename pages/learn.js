bf39349e10f0a75430f1584f5391b9fe92291195
Ryan Gregory <ryangregory.dev@outlook.com>
2021-05-03 11:05:26 +0100

    attempt 3

--- pages/learn.js
+++ pages/learn.js
@@ -5,7 +5,12 @@ import SEO from '../components/SEO';
 export default function Learn() {
   // attempt at focusing on an element for mobile compatibility
   useEffect(() => {
-    document.querySelectorAll("img[alt='wP']")[0].click();
+    const checkExist = setInterval(() => {
+      if (document.querySelectorAll("img[alt='wP']")[0]) {
+        document.querySelectorAll("img[alt='wP']")[0].click();
+        clearInterval(checkExist);
+      }
+    }, 100);
   });
 
   return (
